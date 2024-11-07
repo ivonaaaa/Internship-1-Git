5aa627f344f032035f37f215b3d8ab2a6ec12417
ivonaaaa <ivonaa.e@gmail.com>
2024-11-07 19:20:24 +0100

    Add Zadatak4

---
+++ Zadatak4/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite broj redova: ");
+        int n = int.Parse(Console.ReadLine());
+
+        //gornji dio dijamanta
+        for (int i = 1; i <= n; i += 2)
+        {
+            //ispis razmaka prije zvjezdica
+            Console.Write(new string(' ', (n - i) / 2));
+            //ispis zvjezdica
+            Console.WriteLine(new string('*', i));
+        }
+
+        //donji dio dijamanta
+        for (int i = n - 2; i >= 1; i -= 2)
+        {
+            //ispis razmaka prije zvjezdica
+            Console.Write(new string(' ', (n - i) / 2));
+            //ispis zvjezdica
+            Console.WriteLine(new string('*', i));
+        }
+    }
+}
